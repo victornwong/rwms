c9c6343af1d16d93428ca1cf5574ed19ef2a2556
victornwong <wongvictor1998@gmail.com>
2014-08-13 12:06:40 +0800

    Bytecomp.xml with build things

---
+++ TestMeCode.java
@@ -0,0 +1,18 @@
+package org.victor;
+
+import java.util.*;
+import java.io.*;
+
+public class TestMeCode
+{
+	public TestMeCode()
+	{
+		System.out.println("Whatever..");
+	}
+	
+	public static void main(String[] args)
+	{
+		TestMeCode kk = new TestMeCode();
+		System.out.println("TestMeCode()::");
+	}
+}
